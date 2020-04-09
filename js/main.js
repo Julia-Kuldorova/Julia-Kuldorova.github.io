5e9a56050555037918f779b7219a2753fc8da821
Julia-Kuldorova <julia.julivne@gmail.com>
2020-04-09 19:01:07 +0300

    second

--- js/main.js
+++ js/main.js
@@ -1,3 +1,25 @@
 $(function(){
 
+    $(".rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "17px",
+        readOnly: true,
+        ratedFill: "#ffc000"
+    });
+    $(".product__item--rate-star").rateYo({
+        rating: 4.5,
+        starWidth: "15px",
+        readOnly: true,
+        ratedFill: "#ffc000"
+    });
+
+    $(".slider__inner").slick({
+        infinite: true,
+        prevArrow:
+       '<button class="slick-arrow slick-prev"><span class="lnr lnr-chevron-left"></span></button>',
+     nextArrow:
+       '<button class="slick-arrow slick-next"><span class="lnr lnr-chevron-right"></span></button>'
+    });
+
+    var mixer = mixitup(".products__inner-box");
 });
